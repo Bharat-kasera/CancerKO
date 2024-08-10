041554f517ef30a8cb97568370d57d80c47900bf
Bharat Kasera <bharat.codewolf@gmail.com>
2024-08-11 02:04:36 +0530

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./src/utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: 'postgresql://neondb_owner:HX1rqs7OvdLf@ep-dry-rice-a1ircxf9.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+    connectionString:
+    'postgresql://neondb_owner:HX1rqs7OvdLf@ep-dry-rice-a1ircxf9.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+  },
+};
